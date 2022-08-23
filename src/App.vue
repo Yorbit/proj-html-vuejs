04117d6d07b5c9c5d53948dadfb23ab7f8733159
Yorbit <marco.95112@hotmail.it>
2022-08-23 13:05:06 +0200

    test logo-font

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div id="app">
-    <img>
-    <HelloWorld msg=""/>
+    <MainHeader />
+    <BaseMain />
+    <MainFooter />
   </div>
 </template>
 
 <script>
+import MainHeader from "./components/MainHeader.vue";
+import BaseMain from "./components/BaseMain.vue";
+import MainFooter from "./components/MainFooter.vue";
 
 export default {
   name: 'App',
   components: {
-    
+    MainHeader,
+    BaseMain,
+    MainFooter,
   }
 }
+
 </script>
 
 <style lang="scss">
-
+@import "./styles/generals.scss";
 </style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,11 @@
+<template>
+  <header>
+    <section class="">
+      <div class="">
+        <a id="" href="#">
+            <img src="../assets/img/avada-bakery-logo.png" alt="Bakery_Logo"/>
+        </a>
+      </div>
+    </section>
+  </header>
+</template>
